83dee720ab4c8c6b8218e050643d734776f85226
StanVard0202 <you@example.com>
2021-08-15 20:24:16 +0100

    commi

--- script.js
+++ script.js
@@ -543,4 +543,25 @@ function roll6(){
 }
 
 
-var someVarName = localStorage.getItem("someVarKey");
+var mode = getStoredValue('myPageMode');
+
+function buttonClick(mode) {
+    mode = mode;
+    storeValue('myPageMode', mode);
+}
+
+function storeValue(key, value) {
+    if (localStorage) {
+        localStorage.setItem(key, value);
+    } else {
+        $.cookies.set(key, value);
+    }
+}
+
+function getStoredValue(key) {
+    if (localStorage) {
+        return localStorage.getItem(key);
+    } else {
+        return $.cookies.get(key);
+    }
+}
